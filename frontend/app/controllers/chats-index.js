ac71fb0d457b7969a20d700fc93e579f03917fb5
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2014-11-20 06:02:31 +0800

    added polling object

--- frontend/app/controllers/chats-index.js
+++ frontend/app/controllers/chats-index.js
@@ -4,23 +4,30 @@ export default Ember.ArrayController.extend({
   sortProperties : ['created_at'],
   sortAscending  : false,
   newMessage     : '',
+  lastMessage    : function() {
+    return this.get('arrangedContent.first');
+  }.property('arrangedContent.@each'),
   actions: {
     sendMessage: function() {
       var self = this;
-      var str = localStorage.getItem('ember_simple_auth:session');
-      if (!Em.isEmpty(str)) {
-        var userId = JSON.parse(str).userId;
-        this.store.find('user', userId).then(function(user) {
-          self.store.createRecord('chat', {
-            message : self.get('newMessage'),
-            'user'  : user
-          }).save().then(function() {
-            self.set('newMessage', '');
-          }, function() {
-            alert('cannot save!');
+      if (!Ember.isEmpty(self.get('newMessage'))) {
+        var str = localStorage.getItem('ember_simple_auth:session');
+        if (!Em.isEmpty(str)) {
+          var userId = JSON.parse(str).userId;
+          this.store.find('user', userId).then(function(user) {
+            self.store.createRecord('chat', {
+              message : self.get('newMessage'),
+              'user'  : user
+            }).save().then(function() {
+              self.set('newMessage', '');
+            }, function() {
+              alert('cannot save!');
+            });
           });
-        });
+        } else {
+        }
       } else {
+        alert('please insert text');
       }
     }
   }
--- frontend/app/routes/chats-index.js
+++ frontend/app/routes/chats-index.js
@@ -1,8 +1,27 @@
 import Em from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import Pollster from 'frontend/objects/pollster';
 
 export default Em.Route.extend(AuthenticatedRouteMixin, {
+	polling : false,
+	setupController: function (controller, model) {
+		var self = this;
+		if (Em.isNone(this.get('pollster'))) {
+			this.set('pollster', Pollster.create({
+				onPoll: function() {
+					self.toggleProperty('polling');
+					controller.set('polling', self.get('polling'));
+					console.log('polling... '+self.get('polling'));
+				}
+			}));
+		}
+		this.get('pollster').start();
+		controller.set('content', model);
+	},
 	model: function() {
 		return this.store.find('chat');
+	},
+	deactivate: function() {
+		this.get('pollster').stop();
 	}
 });
